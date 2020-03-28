f80625b004e55171b0058d7fe19b6083937820ce
maiamea <maiameawork@gmail.com>
2020-03-28 19:14:39 +0900

    ルーティングにapiを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  # resources :basal_body_temperatures
-  get 'basal_body_temperatures' => 'basal_body_temperatures#index'
-  get 'basal_body_temperatures/new' => 'basal_body_temperatures#new'
-  post 'basal_body_temperatures' => 'basal_body_temperatures#create'
-  get 'basal_body_temperatures/:id/edit' => 'basal_body_temperatures#edit'
-  put 'basal_body_temperatures/:id' => 'basal_body_temperatures#update'
-  delete 'basal_body_temperatures/:id' => 'basal_body_temperatures#destroy'
-  
+  namespace "api" do
+    # resources :basal_body_temperatures
+    get "basal_body_temperatures" => "basal_body_temperatures#index"
+    get "basal_body_temperatures/new" => "basal_body_temperatures#new"
+    post "basal_body_temperatures" => "basal_body_temperatures#create"
+    get "basal_body_temperatures/:id/edit" => "basal_body_temperatures#edit"
+    put "basal_body_temperatures/:id" => "basal_body_temperatures#update"
+    delete "basal_body_temperatures/:id" => "basal_body_temperatures#destroy"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
