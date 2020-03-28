3f1ad0812398c9e4641784c04e940bffaddc3470
maiamea <maiameawork@gmail.com>
2020-03-28 19:03:43 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
-  get 'basal_body_temperatures/index'
-  get 'basal_body_temperatures/new'
-  get 'basal_body_temperatures/create'
-  get 'basal_body_temperatures/edit'
-  get 'basal_body_temperatures/update'
-  get 'basal_body_temperatures/destroy'
-  get 'basal_body_temperature/index'
-  get 'basal_body_temperature/new'
-  get 'basal_body_temperature/create'
-  get 'basal_body_temperature/edit'
-  get 'basal_body_temperature/update'
-  get 'basal_body_temperature/destroy'
+  # resources :basal_body_temperatures
+  get 'basal_body_temperatures' => 'basal_body_temperatures#index'
+  get 'basal_body_temperatures/new' => 'basal_body_temperatures#new'
+  post 'basal_body_temperatures' => 'basal_body_temperatures#create'
+  get 'basal_body_temperatures/:id/edit' => 'basal_body_temperatures#edit'
+  put 'basal_body_temperatures/:id' => 'basal_body_temperatures#update'
+  delete 'basal_body_temperatures/:id' => 'basal_body_temperatures#destroy'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
